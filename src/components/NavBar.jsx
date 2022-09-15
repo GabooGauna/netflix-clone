bfcdac12d9db664287eb8a402f69f284a3417111
Gauna Cesar Gabriel <cesarggauna@gmail.com>
2022-09-15 16:30:10 -0300

    navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -37,11 +37,11 @@ const NavBar = () => {
       ) : (
         <div>
           <Link to="/login">
-            <button className="text-white pr-4">Sing In</button>
+            <button className="text-white pr-4">Sign In</button>
           </Link>
           <Link to="/signup">
             <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-              Sing Up
+              Sign Up
             </button>
           </Link>
         </div>
